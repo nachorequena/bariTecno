0d969b54eeb0d6cd639a1c7f9ce2f534710caf37
nachorequena <nachorequena2004@gmail.com>
2024-03-23 17:41:44 -0300

    proyecto final-Requena Ignacio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import CartContextProvider from "./context/CartContext";
-import { AppRouter } from "./router/appRouter";
+import { AppRouter } from "./router/AppRouter";
 const App = () => {
   return (
     <BrowserRouter>
