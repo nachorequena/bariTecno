4f31a5176dee320f74d9d7e52ef86fb78fe51c5b
nachorequena <nachorequena2004@gmail.com>
2024-03-23 17:38:46 -0300

    correcciones

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 
-const CartContext = createContext();
+export const CartContext = createContext();
 
 const CartContextProvider = ({ children }) => {
   const [cart, setCart] = useState(
