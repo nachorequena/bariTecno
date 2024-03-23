b46eb409b7fc1ebbcd506271e1fc5b18ec46cb81
nachorequena <nachorequena2004@gmail.com>
2024-03-23 17:15:56 -0300

    correccion de ruta

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
-import { Footer } from "./Footer";
 
 export const Layout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
-      <Footer />
     </>
   );
 };
