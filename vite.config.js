818ff98b8eaa529bc49c9920eaa57d0f8d5c24b7
nachorequena <nachorequena2004@gmail.com>
2024-06-26 03:02:00 -0300

    errores corregidos

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,18 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Ajusta esto si necesitas un límite mayor
   },
 });
